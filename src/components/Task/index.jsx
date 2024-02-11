22d7055ad3370ef483baaaee28bbd2c42796c700
mfjaconis <matheusjaconis@gmail.com>
2024-02-11 16:45:27 -0300

    adicionando o botão editar / salvar e criando suas respectivas funções

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -1,25 +1,54 @@
+import { useState } from "react";
+import { useRef } from "react"
 import styles from "./styles.module.css";
 
 
 export function Task(props) {
+  const [editedTitle, setEditedTitle] = useState(props.task.title);
+
   return (
     <div className={styles.container}>
-      {!props.task.isCompleted && (
-        <input  type="radio" onClick={() => props.handleCompleteTask(props.task.id)}/>)
-      }
-      <p
-        className={`${styles.title} ${
-          props.task.isCompleted && styles.completed
-        }`}
-      >
-        {props.task.id} - {props.task.title} 
-      </p>
+      <div className={styles.content}>
+        {!props.task.isCompleted && (
+          <input type="radio" onClick={() => props.handleCompleteTask(props.task.id)} />)
+        }
+        {props.editing ? (
+          <input
+            className={styles.input}
+            type="text"
+            value={editedTitle}
+            onChange={(e) => setEditedTitle(e.target.value)}
+            autoFocus 
+          />
+        ) : (
+          <p
+            className={`${styles.title} 
+          ${props.task.isCompleted && styles.completed}`}
+          >
+            {props.task.id} - {props.task.title}
+          </p>
+        )
+        }
+      </div>
 
-      <button onClick={() => props.handleDeleteTask(props.task.id)}>
-        Excluir
-      </button>
-    
-    
+      <div>
+        <button className={styles.button} onClick={() => props.handleDeleteTask(props.task.id)}>
+          Excluir
+        </button>
+        {props.editing ? (
+          <button className={styles.button} onClick={() => props.handleSaveEditeTask(props.task.id, editedTitle)}>
+            Salvar
+          </button>
+        ) : (
+          <button className={styles.button} onClick={() => props.handleEditTask(props.task.id)}>
+            Editar
+          </button>
+        )
+        }
+      </div>
+
+
+
     </div>
   );
 }
